8773b4a713b73d1be13e0d36cf9269c18b2b03e8
arnoBruynseels <11308331@student.pxl.be>
2016-10-19 20:45:55 +0200

    start security

--- 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/ApiRestController.java
+++ 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/ApiRestController.java
@@ -1,36 +1,58 @@
 package be.pxl.vegisens.application;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 public class ApiRestController 
 {
+   
+    
+
+    
+    /*private static void listAllUsers()
+    {
+        RestTemplate restTemplate = new RestTemplate(); 
+         
+        HttpEntity<String> request = new HttpEntity<String>(getHeaders());
+        
+        ResponseEntity<List> response = restTemplate.exchange(REST_SERVICE_URI+"/api/", HttpMethod.GET, request, List.class);
+        
+        List<LinkedHashMap<String, Object>> usersMap = (List<LinkedHashMap<String, Object>>)response.getBody();
+         
+        if(usersMap!=null)
+        {
+            for(LinkedHashMap<String, Object> map : usersMap)
+            {
+                System.out.println("User : id="+map.get("id")+", Name="+map.get("name")+", Age="+map.get("age")+", Salary="+map.get("salary"));;
+            }
+        }
+        else
+        {
+            System.out.println("No user exist----------");
+        }
+    }*/
+    
 	    @Autowired
 	    private IGrowableItemService repository;
-
-	    @GetMapping("/api")
-	    public String getGrowableItems() 
-	    {
-	    	List<GrowableItem> growableItemList = repository.getAllGrowableItems();
-	    	
-	         return (growableItemList.get(0)).getDescription();
-	    }
 	    
-	    @GetMapping("/api2")
-	    public List<GrowableItem> getGrowableItems2() 
-	    {	    	
-	         return repository.getAllGrowableItems();
+	    
+	    @GetMapping("/api")
+	    public List<GrowableItem> getGrowableItems1() 
+	    {	    		       
+	        return repository.getAllGrowableItems();
 	    }
 	    	    
-	    //@GetMapping("/text")
-	    @RequestMapping(value="/text", method= RequestMethod.GET)
-	    public String getString()
-	    {
-	    	return "It Works";
-	    }
 }
--- 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/VegiSensApplication.java
+++ 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/VegiSensApplication.java
@@ -1,18 +1,63 @@
 package be.pxl.vegisens.application;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan
 @EnableJpaRepositories
+@EnableGlobalMethodSecurity(securedEnabled=true)
 public class VegiSensApplication {
 
+	 public static final String REST_SERVICE_URI = "http://localhost:8081/vegisens";
+    /*
+     * Add HTTP Authorization header, using Basic-Authentiscation to send user-credentials.
+     */
+    private static HttpHeaders getHeaders()
+    {
+        String plainCredentials="arno:pxl123";
+        String base64Credentials = new String(Base64.encode(plainCredentials.getBytes()));
+         
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+    
+    public static void getAllGrowableItems()
+    {
+        RestTemplate restTemplate = new RestTemplate(); 
+        
+        HttpEntity<String> request = new HttpEntity<String>(getHeaders());
+        
+        ResponseEntity<List> response = restTemplate.exchange(REST_SERVICE_URI+"/api/", HttpMethod.GET, request, List.class);
+                
+		List<GrowableItem>listOfGrowableItems = (List<GrowableItem>)response.getBody();
+		
+		for (GrowableItem growableItem : listOfGrowableItems) 
+		{
+			System.out.println(growableItem.getName());
+		}
+    }
+    
 	public static void main(String[] args)
-	{
+	{		
 		SpringApplication.run(VegiSensApplication.class, args);
+		
+		//getAllGrowableItems();
 	}
 }
 
