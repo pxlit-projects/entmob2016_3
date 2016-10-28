15e3929be694ac72ebf4b8932ac75da6434c63fa
Alessio Costa <Alessio Costa>
2016-10-28 09:32:15 +0200

    fix to support lambda, added aspect colors

--- 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/VegiSensAspect.java
+++ 1 - VegiSens Java Eclipse/vegisens/src/main/java/be/pxl/vegisens/application/VegiSensAspect.java
@@ -6,9 +6,12 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static be.pxl.vegisens.application.LogColor.ANSI_GREEN;
+import static be.pxl.vegisens.application.LogColor.ANSI_RESET;
+
 @Component
 @Aspect
-public class VegiSensAscept {
+public class VegiSensAspect {
 
 	@Autowired
 	private JMSSender sender;
@@ -17,17 +20,21 @@ public class VegiSensAscept {
 	public Object longTimeMethod(ProceedingJoinPoint pjp) throws Throwable
 	{
 		long start = System.currentTimeMillis();
-		
-		sender.sendInformation("[Before Executing]");
-		
+
+		sender.sendInformation(makeGreen("[Before Executing]"));
+
 		Object returnValue = pjp.proceed();
 		
-		sender.sendInformation("[After Executing]");
+		sender.sendInformation(makeGreen("[After Executing]"));
 		
 		long timeElapsed = System.currentTimeMillis() - start;
 		
-		sender.sendInformation("[EXECUTED]: Method execution time : " + timeElapsed + " miliseconds");
+		sender.sendInformation(makeGreen("[EXECUTED]: Method execution time : " + timeElapsed + " miliseconds"));
 		
 		return returnValue;	
+	}
+
+	private String makeGreen(String str) {
+		return ANSI_GREEN + str + ANSI_RESET;
 	}
 }
